32f41c9baa550ee4027efa68194c740ef7075c4e
Thomas Hoggard <mail@thomashoggard.com>
2022-01-16 13:39:25 -0500

    feat: add custom select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <CustomSelect>
+        {displayedValue}
+        <IconWrapper style={{ "--size": "24px" }}>
+          <Icon id="chevron-down" strokeWidth="1" size={24} />
+        </IconWrapper>
+      </CustomSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  appearance: none;
+  opacity: 0;
+`;
+
+const CustomSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 52px 16px 12px;
+  font-size: (16 / 16) rem;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 5px auto ${COLORS.primary};
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  height: var(--size);
+  width: var(--size);
+  pointer-events: none;
+`;
 
 export default Select;
